2122155043f0d1dac861eeb81cea64e1a5847778
red1 <red1@localhost>
2018-03-15 18:23:28 +0800

    refactor and migration record not active after process

--- src/org/wms/model/X_WM_Migration.java
+++ src/org/wms/model/X_WM_Migration.java
@@ -15,7 +15,7 @@
  * or via info@compiere.org or http://www.compiere.org/license.html           *
  *****************************************************************************/
 /** Generated Model - DO NOT CHANGE */
-package org.my.model;
+package org.wms.model;
 
 import java.math.BigDecimal;
 import java.sql.ResultSet;

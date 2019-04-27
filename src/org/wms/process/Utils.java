d10dc503752ad5dad5226132e597f951b438aefc
root <root@localhost>
2019-04-27 15:54:50 +0800

    Recalculation of EmptyStorage Vacant Capacity.

--- src/org/wms/process/Utils.java
+++ src/org/wms/process/Utils.java
@@ -243,12 +243,12 @@ public class Utils {
 	 * @param dsline
 	 * @param empty
 	 */
-	public void calculatePercentageVacant(MWM_DeliveryScheduleLine dsline,MWM_EmptyStorage empty) {
-		if (!dsline.isReceived())
+	public void calculatePercentageVacant(boolean received,MWM_EmptyStorage empty) {
+		if (!received)
 			return;//future, do not want to affect statistics of EmptyStorage
-		empty.setPercentage((empty.getAvailableCapacity().divide(empty.getVacantCapacity(),4,BigDecimal.ROUND_HALF_UP)).multiply(Env.ONEHUNDRED));
+		empty.setPercentage((empty.getAvailableCapacity().divide(empty.getVacantCapacity(),2,BigDecimal.ROUND_HALF_UP)).multiply(Env.ONEHUNDRED));
 		//set is Full if 0% vacant
-		if (empty.getPercentage().compareTo(Env.ZERO)==0)
+		if (empty.getPercentage().compareTo(Env.ZERO)<=0)
 			empty.setIsFull(true);
 		else
 			empty.setIsFull(false);
